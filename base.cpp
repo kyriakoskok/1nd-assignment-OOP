71a8d85e4a0632007e1f84315b70a831ca7cb9df
kyriakos <int02719@uoi.gr>
2023-11-16 17:54:58 +0200

    .

--- base.cpp
+++ base.cpp
@@ -7,6 +7,274 @@
 
 
 
+void Pomodoro::startSession(Pomodoro& c)
+{
+    char x = '0';
+    while ((x!= ' ')&&( x != 'q'))
+    {
+        cout << "press spase to start the pomodoro or press q to exit" << endl;
+        x = _getch();
+        
+    }
+    if (x==' ')
+    {
+
+//-------------------------------------------------------        
+        
+
+    char userInput;
+    bool timerRunning = true;
+    int remainingTime = c.WorkDuration; 
+
+    while (remainingTime > 0) {
+        if (timerRunning) {
+            int minutes = remainingTime / 60;
+            int seconds = remainingTime % 60;
+
+            system("cls"); // Clear the console screen
+
+            cout << "Timer: " << minutes << " minutes " << seconds << " seconds" << endl;
+
+            // Sleep for 1 second
+            this_thread::sleep_for(chrono::seconds(1));
+
+            remainingTime--;
+        }
+
+        if (_kbhit()) {
+            userInput = _getch();
+            if (userInput == 'p') {
+                timerRunning = !timerRunning;
+            } else if (userInput == 'q') {
+                break;
+            } 
+        }
+    }
+
+    if (remainingTime == 0) {
+        cout << "Timer has finished!" << endl;
+    } else {
+        cout << "Timer stopped." << endl;
+    }
+        
+
+
+
+
+
+
+
+
+//------------------------------------------------------------------------
+        
+        if (remainingTime==0)
+        {
+            add_sessionsCompleted();
+            add_totalWorkTime(get_WorkDuration());
+        }
+        else
+        {
+            cout <<"ok :)"<< endl;
+            add_totalWorkTime(get_WorkDuration()-remainingTime);
+        }
+        
+        
+        cout <<"Do you want a break? Press spase..."<<endl;
+        x = _getch();
+        if (x==' ') c.startBreak(c);
+        else cout << "ok :)"<<endl;
+    }
+    else if (x=='q')
+    {
+        cout << "ok :)"<<endl;
+    }
+}
+
+void Pomodoro::startBreak(Pomodoro& c)
+{
+    char x = '0';
+    while ((x!= ' ')&&( x != 'q'))
+    {
+        cout << "press spase to start the break time or press q to exit" << endl;
+        x = _getch();
+    }
+    if (x==' ')
+    {
+        
+
+
+//-------------------------------------------------------        
+    char userInput;
+    bool timerRunning = true;
+    int remainingTime = c.BreakDuration; 
+
+    while (remainingTime > 0) {
+        if (timerRunning) {
+            int minutes = remainingTime / 60;
+            int seconds = remainingTime % 60;
+
+            system("cls"); // Clear the console screen
+
+            cout << "Timer: " << minutes << " minutes " << seconds << " seconds" << endl;
+
+            // Sleep for 1 second
+            this_thread::sleep_for(chrono::seconds(1));
+
+            remainingTime--;
+        }
+
+        if (_kbhit()) {
+            userInput = _getch();
+            if (userInput == 'p') {
+                timerRunning = !timerRunning;
+            } else if (userInput == 'q') {
+                break;
+            } 
+        }
+    }
+
+    if (remainingTime == 0) {
+        cout << "Timer has finished!" << endl;
+    } else {
+        cout << "Timer stopped." << endl;
+    }
+
+
+//------------------------------------------------------------------
+        if (remainingTime!=0)
+        {
+            cout << "ok :)"<<endl;
+        }
+       
+        cout <<"Do you want one more Pomodoro? Press spase..."<<endl;
+        x = _getch();
+        if (x==' ') c.startSession(c);
+        else cout << "ok :)"<<endl;
+    }
+    else if (x=='q')
+    {
+        cout << "ok :)"<<endl;
+    }
+
+}
+
+void Pomodoro::endSession(Pomodoro& c,int x)
+{
+    c.sessionsCompleted ++;
+    c.totalWorkTime += x;
+}
+
+void Pomodoro::getStatistics( Pomodoro& c )
+{
+    Statistics_menu(c);
+}
+
+void Pomodoro::Time_settings( Pomodoro& c)
+{
+
+    int i;
+    char  x=0;
+    while (x!='4')
+    {
+        system("cls"); // Clear the console screen
+        Time_settingsmenu(c);
+        x = _getch();
+        switch (x)
+        {
+        case '1':
+        cout << "WorkDuration:";
+        
+        cin >> i;
+        if (i<0)
+        {
+            cout << "Wrong answer! The number mast be biger then 0" << endl;
+        }
+        else
+        {
+            c.WorkDuration = i;
+            cout << "ok" << endl;
+        }
+        
+        break;
+       case '2':
+        cout << "BreakDuration:";
+        
+        cin >> i;
+        if (i<0)
+        {
+            cout << "Wrong answer! The number mast be biger then 0" << endl;
+        }
+        else
+        {
+            c.BreakDuration = i;
+            cout << "ok" << endl;
+        }
+        break;
+       case '3':
+        c.WorkDuration = 1500;
+        c.BreakDuration = 300;
+        cout << "The settings have reset" << endl;
+        break;
+       case '4':
+       cout << "ok :)" << endl;
+        break;
+       default:
+        cout << "Wrong answer" << endl ;
+        break;
+       }
+    }
+    
+
+    
+}
+
+
+
+void Pomodoro::set_WorkDuration(int x)
+{
+    if (x>0)WorkDuration=x;
+}
+
+void Pomodoro::set_BreakDuration(int x)
+{
+    if (x>0)BreakDuration=x;
+}
+
+void Pomodoro::add_sessionsCompleted(void)
+{
+    sessionsCompleted++;
+}
+
+void Pomodoro::add_totalWorkTime(int x)
+{
+    if (x>0)totalWorkTime += x;
+}
+
+
+
+int Pomodoro::get_WorkDuration(void)
+{
+    return WorkDuration;
+}
+
+int Pomodoro::get_BreakDuration(void)
+{
+    return BreakDuration;
+}
+
+int Pomodoro::get_sessionsCompleted(void)
+{
+    return sessionsCompleted;
+}
+
+int Pomodoro::get_totalWorkTime(void)
+{
+    return totalWorkTime;
+}
+
+
+
+
 
 void basemenu (void)
 {
@@ -51,173 +319,11 @@ void Statistics_menu (Pomodoro& c)
     cout << "+----------------------------+" << endl;
     cout << "|     Statistics menu        |" << endl;
     cout << "+----------------------------+" << endl;
-    cout << "|  sessionsCompleted:"<<c.get_sessionsCompleted() <<"\t  sec|" << endl;
-    cout << "|  totalWorkTime:"<<c.get_totalWorkTime()<<"\t     |"  << endl;
+    cout << "|  sessionsCompleted:"<<c.get_sessionsCompleted() <<"\t     |" << endl;
+    cout << "|  totalWorkTime:"<<c.get_totalWorkTime()<<"\t  sec|"  << endl;
     cout << "+----------------------------+" << endl;
     cout << "press something to go back...";
     _getch();
 }
 
-/*void Time_settingsmenu (void)
-{
-    
 
-    cout << "+----------------------------+" << endl;
-    cout << "|     Time settings menu     |" << endl;
-    cout << "+----------------------------+" << endl;
-    cout << "| 1.current time settings    |" << endl;
-    cout << "| 2.Change WorkDuration time |" << endl;
-    cout << "| 3.Change BreakDuration time|" << endl;
-    cout << "| 4.restore settings         |" << endl;
-    cout << "| 5.back                     |" << endl;
-    cout << "+----------------------------+" << endl;
-
-
-}
-*/ 
-/*void Time_settings(void)
-{
-
-int x=0;
-while (x!=5)
-{
-    Time_settingsmenu();
-    cin >> x;
-    switch (x)
-    {
-    case 1:
-        {
-            ifstream inputFile("DATA.txt");
-
-
-                if (inputFile.is_open()) 
-                {
-
-                    string workDuration ,breakDuration , sessionsCompleted ,totalWorkTime ;
-
-                    getline(inputFile, workDuration);
-                    getline(inputFile, breakDuration);
-                    getline(inputFile, sessionsCompleted);
-                    getline(inputFile, totalWorkTime);
-                
-                    cout << "workDuration is:" << workDuration << " sec" << endl;//Να το κάνω μετατροπή σε λεπτά
-                    cout << "breakDuration is:" << breakDuration << " sec" << endl;//Να το κάνω μετατροπή σε λεπτά
-
-
-                    inputFile.close(); 
-                } else 
-                {
-                    cout << "Unable to find the data file." << endl;
-                }
-
-        }
-        
-        break;
-    case 2:
-        {
-            string workDuration ,breakDuration , sessionsCompleted ,totalWorkTime ;
-            fstream file("DATA.txt", ios::in | ios::out);
-            if (file.is_open())
-            {
-                
-
-                getline(file, workDuration);
-                getline(file, breakDuration);
-                getline(file, sessionsCompleted);
-                getline(file, totalWorkTime);
-
-                file.close();
-            }
-            else 
-            {
-                cout << "Unable to find the data file." << endl;
-            }
-
-            cin >> workDuration;
-
-            ofstream outputFile("DATA.txt", ios::trunc);
-            outputFile << workDuration << endl << breakDuration << endl << sessionsCompleted << endl << totalWorkTime;
-
-            outputFile.close();
-        }
-        break;
-    case 3:
-        {
-            string workDuration ,breakDuration , sessionsCompleted ,totalWorkTime ;
-            fstream file("DATA.txt", ios::in | ios::out);
-            if (file.is_open())
-            {
-                
-
-                getline(file, workDuration);
-                getline(file, breakDuration);
-                getline(file, sessionsCompleted);
-                getline(file, totalWorkTime);
-
-                file.close();
-            }
-            else 
-            {
-                cout << "Unable to find the data file." << endl;
-            }
-
-            cin >> breakDuration;
-
-            ofstream outputFile("DATA.txt", ios::trunc);
-            outputFile << workDuration << endl << breakDuration << endl << sessionsCompleted << endl << totalWorkTime;
-
-            outputFile.close();
-        }
-        break;    
-    case 4:
-        {
-            ofstream outputFile("DATA.txt", ios::trunc);
-            
-            outputFile  << 1500 << endl << 300 << endl << 0 << endl << 0;
-
-            outputFile.close();
-        }
-        break;
-    case 5:
-        cout << "ok"<< endl ;
-        break;    
-    default:
-        cout << "Wrong answer" << endl ;
-        break;
-    }
-
-}
-}
-
-
-
-*/
-/*void getStatistics(void)
-{
-    
-    ifstream inputFile("DATA.txt");
-
-
-    if (inputFile.is_open()) {
-
-        string workDuration ,breakDuration , sessionsCompleted ,totalWorkTime ;
-
-        getline(inputFile, workDuration);
-        getline(inputFile, breakDuration);
-        getline(inputFile, sessionsCompleted);
-        getline(inputFile, totalWorkTime);
-    
-        cout << "sessionsCompleted is:" << sessionsCompleted << endl;
-        cout << "totalWorkTime is:" << totalWorkTime << endl;
-
-
-        inputFile.close(); 
-    } else {
-        cout << "Unable to find the data file." << endl;
-    }
-
-
-
-
-}
-*/
---
+++ test.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "base.hpp"
+
+TEST_CASE("")
+{
+
+}
