f9c8890170fbb57eefd2ddadab1f6e990cb3b612
kyriakos <int02719@uoi.gr>
2023-11-18 16:57:54 +0200

    stable with catch2 test

--- base.hpp
+++ base.hpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <chrono>//για το seconds()
--- test.cpp
+++ test.cpp
@@ -2,7 +2,47 @@
 #include "catch.hpp"
 #include "base.hpp"
 
-TEST_CASE("")
+TEST_CASE("set_WorkDuration")
 {
-    REQUIRE()
+    Pomodoro c1;
+    c1.set_WorkDuration(5);
+    REQUIRE(c1.get_WorkDuration()==5);  
+    c1.set_WorkDuration(3); 
+    REQUIRE(c1.get_WorkDuration()==3);
+    c1.set_WorkDuration(-7);
+    REQUIRE(c1.get_WorkDuration()==3);
+}
+
+
+TEST_CASE("get_BreakDuration")
+{
+    Pomodoro c1;
+    c1.set_BreakDuration(5);
+    REQUIRE(c1.get_BreakDuration()==5);  
+    c1.set_BreakDuration(3); 
+    REQUIRE(c1.get_BreakDuration()==3);
+    c1.set_BreakDuration(-7);
+    REQUIRE(c1.get_BreakDuration()==3);
+}
+
+TEST_CASE("add_sessionsCompleted")
+{
+    Pomodoro c1;
+    c1.add_sessionsCompleted();
+    REQUIRE(c1.get_sessionsCompleted()==1); 
+    c1.add_sessionsCompleted();
+    REQUIRE(c1.get_sessionsCompleted()==2);
+    c1.add_sessionsCompleted();
+    REQUIRE(c1.get_sessionsCompleted()==3);
+}
+
+TEST_CASE("add_totalWorkTime")
+{
+    Pomodoro c1;
+    c1.add_totalWorkTime(3);
+    REQUIRE(c1.get_totalWorkTime()==3); 
+    c1.add_totalWorkTime(5);
+    REQUIRE(c1.get_totalWorkTime()==8);
+    c1.add_totalWorkTime(-7);
+    REQUIRE(c1.get_totalWorkTime()==8);
 }
